f73bda261c583e90daa2d0f362c5746ade3295ab
IvSem <livansll96@gmail.com>
2023-05-13 10:46:48 +0300

    update  modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,23 @@
 import { AppBar, Layout, TaskList } from 'components';
 import { useEffect } from 'react';
 import { toast, ToastContainer, Zoom } from 'react-toastify';
-import { DynamicAdapt } from 'helpers/adaptive';
+//import { DynamicAdapt } from 'helpers/adaptive';
 
 import 'react-toastify/dist/ReactToastify.css';
 import { useGetAllTasksQuery } from 'redux/tasks/slice/slice';
 import { FilterForm } from './FilterForm/FilterForm';
 
 export const App = () => {
-	const { isLoading: loadingDB } = useGetAllTasksQuery();
+	const { isLoading: isLoadingDB } = useGetAllTasksQuery();
 
 	useEffect(() => {
-		loadingDB && toast.info('Went to the server');
-	}, [loadingDB]);
+		isLoadingDB && toast.info('Went to the server');
+	}, [isLoadingDB]);
 
-	useEffect(() => {
-		const da = new DynamicAdapt('max');
-		da.init();
-	}, []);
+	//useEffect(() => {
+	//	const da = new DynamicAdapt('max');
+	//	da.init();
+	//}, []);
 
 	return (
 		<Layout>
--- src/components/ModaCreateTask/ModalCreateTask.js
+++ src/components/ModaCreateTask/ModalCreateTask.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import css from './ModalCreateTask.module.css';
 import { createPortal } from 'react-dom';
 
@@ -7,18 +7,42 @@ import clsx from 'clsx';
 const MODAL_CREATE = document.querySelector('#modal-create');
 
 export const ModalCreateTask = ({ isOpen, onClose, children }) => {
-	console.log(':>  ModalCreateTask  isOpen:', isOpen);
-	useEffect(() => {}, []);
+	const [isContentVisible, setContentVisible] = useState(false);
+
+	useEffect(() => {
+		const handleKeyDown = event => {
+			if (event.key === 'Escape') {
+				onClose(false);
+			}
+		};
+
+		if (!isOpen) {
+			setTimeout(() => {
+				setContentVisible(false);
+			}, 0);
+		}
+
+		const timeoutId = setTimeout(() => {
+			setContentVisible(true);
+		}, 0);
+
+		document.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			clearTimeout(timeoutId);
+			document.removeEventListener('keydown', handleKeyDown);
+		};
+	}, [isOpen, onClose]);
 
 	return createPortal(
 		<div
-			className={clsx(css.overlay, { [css.active]: isOpen })}
+			className={clsx(css.overlay, { [css.active]: isContentVisible })}
 			onClick={() => {
 				onClose(false);
 			}}
 		>
 			<div
-				className={clsx(css.modalContent, { [css.active]: isOpen })}
+				className={clsx(css.modalContent, { [css.active]: isContentVisible })}
 				onClick={e => {
 					e.stopPropagation();
 				}}
